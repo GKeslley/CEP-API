13cacb33d183449a08906fc7e3f2371447406b58
Guilherme Keslley <guilhermekeslley123@gmail.com>
2022-07-13 22:38:43 -0300

    Javascript

---
+++ script.js
@@ -0,0 +1,31 @@
+const input = document.getElementById("cep");
+const btn = document.querySelector(".button");
+const cepDiv = document.querySelector(".cepText");
+
+function handleClick() {
+  const cepValor = input.value;
+
+  const cep = fetch(`https://viacep.com.br/ws/${cepValor}/json/`);
+  cep
+    .then((valor) => valor.json())
+    .then((r) => {
+      const p = `<p>Localidade: ${r.localidade}/${r.uf}, <p>Bairro:  ${r.bairro}, <p>Logradouro: ${r.logradouro}, <p>CEP: ${r.cep}`;
+      const insertP = p.split(", ").join("</p>");
+      cepDiv.innerHTML = insertP;
+      if (insertP) {
+        cepDiv.classList.add("cepText");
+      }
+      if (r.erro) {
+        cepDiv.innerHTML = "Endereço não encontrado";
+        cepDiv.classList.remove("cepText");
+      }
+    })
+    .catch(() => {
+      cepDiv.innerHTML = "CEP inválido";
+      if ((cepDiv.innerHTML = "CEP inválido")) {
+        cepDiv.classList.remove("cepText");
+      }
+    });
+}
+
+btn.addEventListener("click", handleClick);
